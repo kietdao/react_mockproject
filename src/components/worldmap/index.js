c1952afd8a66ca711009de6067516e99fb40151d
kietdao <43434606+kietdao@users.noreply.github.com>
2022-07-05 18:32:47 +0700

    edit world map

--- src/components/worldmap/index.js
+++ src/components/worldmap/index.js
@@ -1,54 +1,78 @@
-import {useState, useEffect} from 'react'
 import { useSelector } from 'react-redux';
 import Highcharts from 'highcharts'
 import HighchartsMap from "highcharts/modules/map";
 import HighchartsReact from 'highcharts-react-official'
-import map from "@highcharts/map-collection/custom/world-highres.geo.json";
-import axios from 'axios'
+import map from "@highcharts/map-collection/custom/world.geo.json";
 HighchartsMap(Highcharts);
 export default function WorldMap() {
-  const mapData = useSelector(state => state.countries?.countryList)
-  console.log(mapData)
+  const data = useSelector(state => state.countries?.countryList)
+  const dataEnableFormat = JSON.parse(JSON.stringify(data))
+  const formattedData = dataEnableFormat.map(country => {
+    if(country.countryName === 'USA') {
+      country.countryName = 'United States of America'
+    } else if(country.countryName === 'UK') {
+      country.countryName = 'United Kingdom'
+    } else if(country.countryName === 'Libyan Arab Jamahiriya') {
+      country.countryName = 'Libya'
+    } else if(country.countryName === 'Congo') {
+      country.countryName = 'Republic of Congo'
+    } else if(country.countryName === `Côte d'Ivoire`) {
+      country.countryName = 'Ivory Coast'
+    } else if(country.countryName === 'DRC') {
+      country.countryName = 'Democratic Republic of the Congo'
+    } else if(country.countryName === 'Tanzania') {
+      country.countryName = 'United Republic of Tanzania'
+    } else if(country.countryName === 'N. Korea') {
+      country.countryName = 'North Korea'
+    } else if(country.countryName === 'S. Korea') {
+      country.countryName = 'South Korea'
+    } else if(country.countryName === `Lao People's Democratic Republic`) {
+      country.countryName = 'Laos'
+    }
+    return country
+  })
   const options = {
     chart: {
+      map: map,
       borderWidth: 1,
-      map: map
     },
-  
     title: {
-      text: "World population 2013 by country"
+      text: "Covid-19 World Map"
     },
-  
     subtitle: {
-      text: "Demo of Highcharts map with bubbles"
+      text: "Cofirmed Cases"
     },
-  
     legend: {
       enabled: false
     },
-  
     mapNavigation: {
       enabled: true,
       buttonOptions: {
         verticalAlign: "bottom"
       }
     },
-  
+    plotOptions: {
+      map: {
+        states: {
+          hover: {
+            color: 'yellow'
+          }
+        }
+      }
+    },
     series: [
       {
         name: "Countries",
-        color: "#E0E0E0",
-        enableMouseTracking: true
+        color: "#E0E0E",
+        enableMouseTracking: false,
       },
       {
         type: "map",
-        name: "Population 2016",
-        joinBy: null,
-        data: mapData,
-        minSize: 5,
-        maxSize: "50%",
+        name: "Confirmed Cases",
+        data: formattedData,
+        joinBy: ['name','countryName'],
         tooltip: {
-          pointFormat: "{point.properties.name}: {point.confirmed} thousands"
+          pointFormat: `{point.properties.name}: {point.confirmed} people`
         }
       }
     ]
